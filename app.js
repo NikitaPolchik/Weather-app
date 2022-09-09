d3ff4e6d8899df9f7c7c5d7177fdd106c92b4cad
Your Name <you@example.com>
2022-09-09 23:52:27 +0300

    icons added, f to c changable

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ window.addEventListener("load", () => {
       );
       let temperatureDegree = document.querySelector(".temperature-degree");
       let locationTemezone = document.querySelector(".location-timezone");
+      let temperatureSection = document.querySelector(".temperature");
+      let temperatureSpan = document.querySelector(".temperature span");
 
       // using api from openweathermap.org
       const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=13ab6440540ca02b0e392d739edb785d&units=imperial`;
@@ -30,13 +32,45 @@ window.addEventListener("load", () => {
           locationTemezone.textContent = data.name;
           temperatureDescription.textContent = data.weather[0].description;
 
-          //define icons from API
+          //define icons from API, adapt icons to openWeatherMap site data
           function defineIcons(skyconsImage) {
             let weatherCondition = data.weather[0].description;
-            if (weatherCondition == "few clouds") {
+
+            if (weatherCondition == "few clouds" && timeAndDate() < 6) {
+              skyconsImage = Skycons.PARTLY_CLOUDY_NIGHT;
+            } else if (weatherCondition == "few clouds" && timeAndDate() >= 6) {
               skyconsImage = Skycons.PARTLY_CLOUDY_DAY;
-              return skyconsImage;
+            } else if (weatherCondition == "clear sky" && timeAndDate() < 6) {
+              skyconsImage = Skycons.CLEAR_NIGHT;
+            } else if (weatherCondition == "clear sky" && timeAndDate() >= 6) {
+              skyconsImage = Skycons.CLEAR_DAY;
+            } else {
+              switch (weatherCondition) {
+                case "fog":
+                  skyconsImage = Skycons.FOG;
+                  break;
+                case "scattered clouds":
+                  skyconsImage = Skycons.CLOUDY;
+                  break;
+                case "broken clouds":
+                  skyconsImage = Skycons.CLOUDY;
+                  break;
+                case "shower rain":
+                  skyconsImage = Skycons.RAIN;
+                  break;
+                case "rain":
+                  skyconsImage = Skycons.RAIN;
+                  break;
+                case "thunderstorm":
+                  skyconsImage = Skycons.SNOW;
+                  break;
+                case "snow":
+                  skyconsImage = Skycons.SNOW;
+                  break;
+              }
             }
+
+            return skyconsImage;
           }
 
           //set icons
@@ -45,6 +79,7 @@ window.addEventListener("load", () => {
             skycons.add("icon1", defineIcons());
             skycons.play();
           }
+          setIcons();
 
           // current time and date
 
@@ -53,8 +88,20 @@ window.addEventListener("load", () => {
             let hour = date.getHours();
             return hour;
           }
-          console.log(timeAndDate());
-          setIcons();
+
+          // !!!!
+
+          // change temperature to Farenheit/Celsius
+
+          temperatureSection.addEventListener("click", () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+            } else {
+              temperatureSpan.textContent = "F";
+            }
+
+            // !!!!
+          });
         });
     });
   }
