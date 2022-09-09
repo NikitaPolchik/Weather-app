9ac9f9b063a9b4f2a98931ed749f170fe4cbb2d9
Your Name <you@example.com>
2022-09-10 00:15:10 +0300

    finished app

--- app.js
+++ app.js
@@ -89,18 +89,62 @@ window.addEventListener("load", () => {
             return hour;
           }
 
-          // !!!!
+          // formula from F to C
+
+          let celsius = (temp - 32) * (5 / 9);
 
           // change temperature to Farenheit/Celsius
 
           temperatureSection.addEventListener("click", () => {
             if (temperatureSpan.textContent === "F") {
               temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = decimalAdjust(
+                "floor",
+                celsius,
+                -1
+              );
+
+              //!!! not my code
+              // some stolen code for decimal celsius number
+              function decimalAdjust(type, value, exp) {
+                type = String(type);
+                if (!["round", "floor", "ceil"].includes(type)) {
+                  throw new TypeError(
+                    "The type of decimal adjustment must be one of 'round', 'floor', or 'ceil'."
+                  );
+                }
+                exp = Number(exp);
+                value = Number(value);
+                if (exp % 1 !== 0 || Number.isNaN(value)) {
+                  return NaN;
+                } else if (exp === 0) {
+                  return Math[type](value);
+                }
+                const [magnitude, exponent = 0] = value.toString().split("e");
+                const adjustedValue = Math[type](
+                  `${magnitude}e${exponent - exp}`
+                );
+                // Shift back
+                const [newMagnitude, newExponent = 0] = adjustedValue
+                  .toString()
+                  .split("e");
+                return Number(`${newMagnitude}e${+newExponent + exp}`);
+              }
+
+              // Decimal round
+              const round10 = (value, exp) =>
+                decimalAdjust("round", value, exp);
+              // Decimal floor
+              const floor10 = (value, exp) =>
+                decimalAdjust("floor", value, exp);
+              // Decimal ceil
+              const ceil10 = (value, exp) => decimalAdjust("ceil", value, exp);
+
+              //!!!
             } else {
               temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temp;
             }
-
-            // !!!!
           });
         });
     });
